5fde14f3657616bbc8ed3b0ba00f2d647bc8f493
Tungnguyen038 <19130260@st.hcmuaf.edu.vn>
2022-01-02 22:06:39 +0700

    hom nay da lam xong phan admin nhasanxuat

---
+++ ThucHanh1112/ThucHanh1112/Areas/Admin/Controllers/NhaSanXuatAdminController.cs
@@ -0,0 +1,92 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThucHanh1112.Models.BUS;
+
+namespace ThucHanh1112.Areas.Admin.Controllers
+{
+    public class NhaSanXuatAdminController : Controller
+    {
+        // GET: Admin/NhaSanXuat
+        public ActionResult Index()
+        {
+            var ds = NhaSanXuatBUS.DanhSach();
+            return View(ds);
+        }
+
+        // GET: Admin/NhaSanXuat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/NhaSanXuat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaSanXuat/Create
+        [HttpPost]
+        public ActionResult Create( NhaSanXuat nsx)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                NhaSanXuatBUS.ThemNSX(nsx);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaSanXuat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaSanXuat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaSanXuat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaSanXuat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- ThucHanh1112/ThucHanh1112/Models/BUS/NhaSanXuatBUS.cs
+++ ThucHanh1112/ThucHanh1112/Models/BUS/NhaSanXuatBUS.cs
@@ -18,5 +18,10 @@ namespace ThucHanh1112.Models.BUS
             var db = new ShopOnlineConnectionDB();
             return db.Query<SanPham>("select * from SanPham where MaNhaSanXuat='" + id +"'");
         }
+        public static void ThemNSX(NhaSanXuat nsx)
+        {
+            var db = new ShopOnlineConnectionDB();
+            db.Insert(nsx);
+        }
     }
 }
